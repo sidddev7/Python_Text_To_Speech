eb75313f3b040c8e49d8d42181113f2204b919ae
siddhu1999 <72236599+siddhu1999@users.noreply.github.com>
2021-10-23 07:21:11 +0530

    Update textToSpeechPython.py
    Changed the tpye of speaking, the program speaks directly without saving the generated audio file

--- textToSpeechPython.py
+++ textToSpeechPython.py
@@ -1,26 +1,25 @@
-# Import the required module for text 
-# to speech conversion
-from gtts import gTTS
-  
-# This module is imported so that we can 
-# play the converted audio
-import os
-  
-# The text that you want to convert to audio
-mytext = 'Welcome to geeksforgeeks!'
-  
-# Language in which you want to convert
-language = 'en'
-  
-# Passing the text and language to the engine, 
-# here we have marked slow=False. Which tells 
-# the module that the converted audio should 
-# have a high speed
-myobj = gTTS(text=mytext, lang=language, slow=False)
-  
-# Saving the converted audio in a mp3 file named
-# welcome 
-myobj.save("welcome.mp3")
-  
-# Playing the converted file
-os.system("mpg321 welcome.mp3")
+# -*- coding: utf-8 -*-
+
+# you will need 2 python libraries 
+#1. pyttx3
+#2. PyPDF2
+
+import pyttsx3
+import PyPDF2
+
+book=open('your pdf file location','rb')
+
+pdfreader=PyPDF2.PdfFileReader(book)
+
+pages=pdfreader.numPages
+#print(pages)
+#select the page number that you want python program to narrate
+page=pdfreader.getPage(30)
+
+text=page.extractText()
+
+speaker=pyttsx3.init()
+
+speaker.say(text)
+
+speaker.runAndWait()
